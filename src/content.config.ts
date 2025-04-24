0b331a9dffee8579fcc50261ff80e1eb01459a96
Serenacula <faylee@hanginggardens.com>
2025-04-24 01:23:21 +0100

    inputting markdown

---
+++ src/content.config.ts
@@ -0,0 +1,20 @@
+import { defineCollection, z } from "astro:content"
+
+import { glob } from "astro/loaders"
+
+const projects = defineCollection({
+    loader: glob({ base: "./src/markdown/projects", pattern: "*.md" }),
+    schema: z.object({
+        title: z.string(),
+        date: z.string(),
+        image: z.string(),
+        link: z.string(),
+        github: z.string(),
+        type: z.union([z.literal("project"), z.literal("post")]),
+        features: z.string().array(),
+        posted: z.boolean(),
+        description: z.string(),
+    }),
+})
+
+export const collections = { projects }
