34b46a5403458c85a741d38c7c19191bfc665dee
Serenacula <faylee@hanginggardens.com>
2025-04-25 15:44:59 +0100

    adjusting links for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://serenacula.github.io",
-    base: "astro-portfolio",
-    outDir: "./docs",
+    site: "https://www.fayleemb.com",
 })
