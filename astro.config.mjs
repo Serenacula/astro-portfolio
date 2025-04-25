5f7f759f76cfa35a8420c0c7ea4bc726da06f61a
Serenacula <faylee@hanginggardens.com>
2025-04-25 14:52:13 +0100

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://serenacula.github.io",
+    base: "astro-portfolio",
     outDir: "./docs",
 })
