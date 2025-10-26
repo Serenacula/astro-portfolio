84ac0de338d9731cc61572df48794e644254b40d
Serenacula <26283806+Serenacula@users.noreply.github.com>
2025-10-26 15:54:46 +0000

    testing removing codesplit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,9 @@ import { defineConfig } from "astro/config"
 // https://astro.build/config
 export default defineConfig({
     site: "https://fayleemb.com",
-    vite: {
-        build: {
-            cssCodeSplit: false,
-        },
-    },
+    // vite: {
+    //     build: {
+    //         cssCodeSplit: false,
+    //     },
+    // },
 })
