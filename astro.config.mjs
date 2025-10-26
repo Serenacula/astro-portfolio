7ee3f2b19bef07d46e25e76748bc59e024b3ce55
Serenacula <26283806+Serenacula@users.noreply.github.com>
2025-10-26 15:26:31 +0000

    testing no codesplit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,9 @@ import { defineConfig } from "astro/config"
 // https://astro.build/config
 export default defineConfig({
     site: "https://fayleemb.com",
+    vite: {
+        build: {
+            cssCodeSplit: false,
+        },
+    },
 })
